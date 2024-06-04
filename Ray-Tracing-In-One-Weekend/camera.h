daed86680780c27775d520a1d9e42f665009eb65
oney2033 <131510065+oney2033@users.noreply.github.com>
2024-06-04 21:30:03 +0800

    添加了可调景深的场景相机

--- Ray-Tracing-In-One-Weekend/camera.h
+++ Ray-Tracing-In-One-Weekend/camera.h
@@ -12,7 +12,14 @@ public:
     int    image_width = 100;  // 渲染图像宽度（以像素数为单位
     int    samples_per_pixel = 10;//每个像素的随机样本总数
     int    max_depth = 10;      //反弹到场景中的最大光线数量
+    
     double vfov = 90;           //垂直视角
+    point3 lookfrom = point3(0, 0, 0); // 点相机的视角
+    point3 lookat = point3(0, 0, -1);  // 点相机的看向
+    vec3   vup = vec3(0, 1, 0);       // 相机向上方向
+
+    double defocus_angle = 0;   //光圈开口角度
+    double focus_dist = 10;     //焦点
 
     void render(const hittable& world) 
     {
@@ -45,7 +52,9 @@ private:
     point3 pixel00_loc;    // 像素 0, 0 的位置
     vec3   pixel_delta_u;  // 像素向右的偏移量
     vec3   pixel_delta_v;  // 像素向下的偏移量
-
+    vec3   u, v, w;        // 相机帧基础向量
+    vec3   defocus_disk_u; //散焦盘的水平半径
+    vec3   defocus_disk_v; //散焦盘的垂直半径
 
     void initialize() {
         image_height = int(image_width / aspect_ratio);
@@ -53,40 +62,49 @@ private:
 
         pixel_samples_scale = 1.0 / samples_per_pixel;
 
-        center = point3(0, 0, 0);
+        center = lookfrom;
 
         // 确定视口尺寸
-        auto focal_length = 1.0;
-        auto viewport_height = 2.0;
         auto theta = degrees_to_radians(vfov);
         auto h = tan(theta / 2);
-        //auto viewport_height = 2 * h * focal_length;
+        auto viewport_height = 2 * h * focus_dist;
         auto viewport_width = viewport_height * (double(image_width) / image_height);
 
-        // 计算沿水平和垂直视口边缘向下的向量
-        auto viewport_u = vec3(viewport_width, 0, 0);
-        auto viewport_v = vec3(0, -viewport_height, 0);
+        // 计算相机坐标系的 u,v,w 单位基向量
+        w = unit_vector(lookfrom - lookat);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
+
+        // 计算沿水平和垂直视口的大小
+        vec3 viewport_u = viewport_width * u;   
+        vec3 viewport_v = viewport_height * -v; 
 
         // 计算从像素到像素的水平和垂直增量向量
         pixel_delta_u = viewport_u / image_width;
         pixel_delta_v = viewport_v / image_height;
 
         // 计算左上角像素的位置
-        auto viewport_upper_left =
-            center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+        auto viewport_upper_left = center - (focus_dist * w) - viewport_u/2 - viewport_v/2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+
+        // 计算相机散焦盘基向量
+        auto defocus_radius = focus_dist * tan(degrees_to_radians(defocus_angle / 2));
+        defocus_disk_u = u * defocus_radius;
+        defocus_disk_v = v * defocus_radius;
+
     }
 
     ray get_ray(int i, int j)const
     {
-        //构造一条来自原点并指向随机采样的光线
+        //构造一条来自散焦盘并指向随机的光线
         //在i和j的周围
         auto offset = sample_square();
         auto pixel_sample = pixel00_loc
                              + ((i + offset.x()) * pixel_delta_u)
                              + ((j + offset.y()) * pixel_delta_v);
 
-        auto ray_origin = center;
+        auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
         auto ray_direction = pixel_sample - ray_origin;
         return ray(ray_origin, ray_direction);
     }
@@ -95,6 +113,13 @@ private:
     {
         //返回[-0.5, 0-.5] - [+0.5, +0.5] 单位正方形中的随机点
         return vec3(random_double() - 0.5, random_double() - 0.5, 0);
+    }
+
+    point3 defocus_disk_sample()const
+    {
+        //返回相机散焦盘中的随机点
+        auto p = random_in_unit_disk();
+        return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
     }
 
 	color ray_color(const ray& r, int depth, const hittable& world)const
